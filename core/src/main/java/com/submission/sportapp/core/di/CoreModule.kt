1dd82130a31cd01f9e758bf69b8874af19970836
Hary <noval.ramadan600@gmail.com>
2022-09-22 09:26:54 +0700

    create encryption data

--- core/src/main/java/com/submission/sportapp/core/di/CoreModule.kt
+++ core/src/main/java/com/submission/sportapp/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.submission.sportapp.core.data.source.remote.RemoteDataSource
 import com.submission.sportapp.core.data.source.remote.network.ApiService
 import com.submission.sportapp.core.domain.repository.ISportsRepository
 import com.submission.sportapp.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,19 +23,30 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<SportsDatabase>().sportsDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("Sports".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             SportsDatabase::class.java, "Sports.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "www.thesportsdb.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/NnupxxWETrHvZNGEnih0b/uMtCHadqgOzzx/jlJnBKI=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
